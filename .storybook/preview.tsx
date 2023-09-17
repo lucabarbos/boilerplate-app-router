a92f149affb3cf1dd52257a23a2ec5a1e81103f0
Lucas Barbosa <signature@lucasbarbosa.dev>
2023-09-17 14:11:28 -0300

    feat: add globalstyles on storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { GlobalStyles } from "../src/styles/global";
+
+export const decorators = [
+	Story => (
+		<>
+			<GlobalStyles />
+			<Story />
+		</>
+	),
+];

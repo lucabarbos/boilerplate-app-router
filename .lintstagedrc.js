714511822ff2f59cadaf346ce9cbea3496403c30
Lucas Barbosa <signature@lucasbarbosa.dev>
2023-09-17 09:43:51 -0300

    chore: add husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	// lint and prettify all files
+	"*{js,jsx,ts,tsx}": filenames => [
+		`prettier --write ${filenames.join(" ")}`,
+		`npm run lint --fix . ${filenames.join(" --file")}`,
+	],
+};
